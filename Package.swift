1873ffebd8107a0d031f1035492c4e608d6274d1
Christian Tietze <me@christiantietze.de>
2022-12-04 17:16:44 +0100

    update Package manifest for macOS 10.12 (min. req. by dependencies)

--- Package.swift
+++ Package.swift
@@ -1,17 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "DiffPerformanceSuite",
+    platforms: [
+        .macOS(.v10_12)
+    ],
     products: [
         .executable(name: "PerformanceTester", targets: ["PerformanceTester"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tonyarnold/Differ.git", .branch("master")),
+        .package(url: "https://github.com/tonyarnold/Differ.git", .branch("main")),
         .package(url: "https://github.com/jflinter/Dwifft.git", .branch("master"))
     ],
     targets: [
         .target(name: "PerformanceTester", dependencies: ["Differ", "Dwifft"], exclude: ["Samples"])
-    ],
-    swiftLanguageVersions: [4]
+    ]
 )
